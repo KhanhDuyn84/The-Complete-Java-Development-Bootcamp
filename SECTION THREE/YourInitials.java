019f6fc84800f302dfa5deb9187217fe7e3da6a2
KhanhDuy <khanhduyn84@gmail.com>
2022-12-13 21:30:55 +0700

    add Section Three & challenge to github

---
+++ SECTION THREE/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("N   N   K   K   DDDD ");
+        System.out.println("NN  N   K  K    D   D");
+        System.out.println("N N N   K K     D   D");
+        System.out.println("N  NN   KK      D   D");
+        System.out.println("N   N   K K     D   D");
+        System.out.println("N   N   K  K    D   D");
+        System.out.println("N   N   K   K   DDDD");
+    }
+}
